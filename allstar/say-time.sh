e97f2ffec524a8c40c7db9d4e4e749a23068e2bf
John Olmsted <john@olmsted.io>
2020-07-16 19:37:43 -0700

    allstar/saytime.sh script takes location argument

--- allstar/say-time.sh
+++ allstar/say-time.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Run this script to announce the time. Make sure you've symlinked the 'saytime.pl' script in this repo so there's
-# a copy at '/usr/local/sbin/saytime.pl', which is what this script looks for.
-(source /usr/local/etc/allstar.env; /usr/bin/nice -19 /usr/bin/perl /usr/local/sbin/saytime.pl $NODE1 > /dev/null)
+# Run this script to announce the time. Make sure you've symlinked the 'saytime.pl'
+# script in this repo so there's a copy at '/usr/local/sbin/saytime.pl', which is what
+# this script looks for.
+#
+# Usage:
+#
+#   ./say-time.sh <location>
+#
+# This will announce the time, and if a location argument is provided, conclude by
+# announcing '<location> time' to help specify time zone. The location argument must be
+# the name of an audio file found in the '/var/lib/asterisk/sounds' directory.
+
+(source /usr/local/etc/allstar.env; /usr/bin/nice -19 /usr/bin/perl /usr/local/sbin/saytime.pl $NODE1 > /dev/null); /usr/sbin/asterisk -rx "rpt playback 51865 ${1}"; /usr/sbin/asterisk -rx 'rpt playback 51865 time'
 
